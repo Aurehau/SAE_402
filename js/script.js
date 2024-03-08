ad232a49a19960df8174a4bc9dda186094b838b1
Aurehau <114416513+Aurehau@users.noreply.github.com>
2024-03-08 11:04:31 +0100

    début mur

--- js/script.js
+++ js/script.js
@@ -1,12 +1,77 @@
-window.onload = function() {
-    var canvas = document.getElementById("c");
-    var ctx = canvas.getContext("2d");
+  let MouseX=396;
+  let MouseY=1296;
   
-    var image = new Image();
-    image.src = "../img/perso.svg";
+  let vitesse=0.05;
   
-    image.onload = function() {
-      // Dessine l'image sur le canvas aux coordonnées x=0, y=0
-      ctx.drawImage(image, 1500, 1220);
-    };
-  };
+  let perso=document.querySelector(".corp");
+  
+  let rondX=396;
+  let rondY=1296;
+  
+  //window.requestAnimationFrame(deplacementPerso);
+  deplacementPerso();
+  
+  document.addEventListener("mousemove", positionMouse)
+  
+  
+  function positionMouse(event){
+      MouseX= event.pageX;
+      MouseY= event.pageY;
+
+      objectifAngle=calculerAngle();
+  }
+  
+  function deplacementPerso(){
+
+        //Déplacement sur X et Y
+        let MouseVX= (MouseX-rondX)*vitesse;
+        let MouseVY=(MouseY-rondY)*vitesse;
+
+        if ((rondX+ MouseVX)>(72+30)&&(rondX+ MouseVX)<(1728-30)) {
+            rondX+= MouseVX;
+        }
+
+        if (((rondY+ MouseVY)>(72+25))&&((rondY+ MouseVY)<(1296-25))) {
+            rondY+= MouseVY;
+        }
+
+        if (((rondY+ MouseVY)>(1296-25))&&((rondX+ MouseVX)>(360-30))) {
+            rondY+= MouseVY;
+        }
+    
+        //rondX+= MouseVX;
+        //rondY+= MouseVY;
+
+        //Rotation
+        var angleRadians = Math.atan2(MouseY - rondY, MouseX - rondX);
+        var angleDegrees = (angleRadians * 180) / Math.PI;
+    
+        perso.style.transform=`translate(${rondX-36}px, ${rondY-36}px) rotate(${90+angleDegrees}deg)`;
+
+
+        //nouvel appel de la fonction
+        window.requestAnimationFrame(deplacementPerso);
+
+      
+  }
+
+
+  function calculerAngle() {
+    // Calculer la différence en X et en Y entre les points B et A
+    var deltaX = MouseX - rondX;
+    var deltaY = MouseY - rondY;
+  
+    // Utiliser atan2 pour trouver l'angle en radians
+    var angleRadians = Math.atan2(deltaY, deltaX);
+  
+    // Convertir l'angle en degrés
+    var angleDegrees = angleRadians * (180 / Math.PI);
+  
+    // Ajouter 360 degrés et prendre le modulo 360 pour obtenir un angle positif
+    angleDegrees = (angleDegrees + 360) % 360;
+  
+    return angleDegrees;
+  }
+
+  //console.log(calculerAngle());
+  
