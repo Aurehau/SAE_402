fa56aeb6619f3957a8bfddb2085bbddfae1d16fa
Aurehau <114416513+Aurehau@users.noreply.github.com>
2024-03-26 08:53:25 +0100

    a

--- js/script2.js
+++ js/script2.js
@@ -1,40 +1,6 @@
-/* ------------------------- */
-/* Version en JavaScript pur */
-/* ------------------------- */
-"use strict";
-window.addEventListener("DOMContentLoaded", (event) => {
-  animate_text();
-});
-// -------------------
-function animate_text() 
-{
-  let delay = 10,
-      delay_start = 0,
-      contents,
-      letters;
+document.querySelector(".Binfo").addEventListener("click",afficheInfo);
 
-  document.querySelectorAll(".animate-text").forEach(function (elem) {
-    contents = elem.textContent.trim();
-    elem.textContent = "";
-    letters = contents.split("");
-    elem.style.visibility = 'visible';
-
-    letters.forEach(function (letter, index_1) {
-      setTimeout(function () {
-        // ---------
-        // effet machine à écrire (SIMPLE)
-        elem.textContent += letter;
-        // ---------
-        // OU :
-        // effet machine à écrire + animation CSS (SPECIAL)
-        /*
-        var span = document.createElement('span');
-        span.innerHTML = letter.replace(/ /,'&nbsp;');
-        elem.appendChild(span);
-*/
-        // ---------
-      }, delay_start + delay * index_1);
-    });    
-    delay_start += delay * letters.length;
-  });
+function afficheInfo(){
+    document.querySelector(".pop-up").classList.toggle("pop-existe");
+    document.querySelector(".pop-up>.bull").classList.toggle("pop-it");
 }
