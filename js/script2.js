031e08edb109bf69c7eccf37a3f14c7d70124af2
chaymabann <chayma.bann@outlook.fr>
2024-04-11 09:42:21 +0200

    update 402

--- js/script2.js
+++
@@ -1,8 +0,0 @@
-document.querySelector(".Binfo").addEventListener("click",afficheInfo);
-document.querySelector(".croix").addEventListener("click",afficheInfo);
-
-function afficheInfo(){
-    document.querySelector(".pop-up").classList.toggle("pop-existe");
-    document.querySelector(".pop-up>.bull").classList.toggle("pop-it");
-    document.querySelector(".pop-up>.croix").classList.toggle("turncroix");
-}
